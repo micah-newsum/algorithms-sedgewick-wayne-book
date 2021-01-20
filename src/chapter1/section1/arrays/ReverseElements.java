313d3ab6bd8cc8f877b8e8f11891df70b7f3e497
Micah Newsum <mnewsum@glenair.com>
2021-01-19 21:10:48 -0800

    Add ReverseElements

---
+++ src/chapter1/section1/arrays/ReverseElements.java
@@ -0,0 +1,25 @@
+package chapter1.section1.arrays;
+
+public class ReverseElements {
+
+	public static void main(String[] args) 
+	{
+		double[] vals = {1.0,3.0,5.0,6.0,3.0,7.0,8.0,2.0,9.0};
+		int N = vals.length;		
+		for (int i = 0; i < N/2; i++)
+		{
+			double temp = vals[i];
+			vals[i] = vals[N-i-1];
+			vals[N-i-1] = temp;
+		}
+		printReverse(vals);
+	}
+	
+	static void printReverse(double vals[])
+	{
+		for (int i = 0; i < vals.length; i++)
+			System.out.println(vals[i]);
+	}
+		
+
+}
