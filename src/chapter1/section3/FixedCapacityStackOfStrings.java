1386b4af16f5ce823594a2a0145a127f7d51af37
Micah Newsum <mnewsum@glenair.com>
2021-02-06 11:54:04 -0800

    Update FixedCapacityStackOfStrings.java

--- src/chapter1/section3/FixedCapacityStackOfStrings.java
+++ src/chapter1/section3/FixedCapacityStackOfStrings.java
@@ -1,10 +1,57 @@
 package chapter1.section3;
 
-public class FixedCapacityStackOfStrings {
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class FixedCapacityStackOfStrings 
+{
+	private String[] a;
+	private int N;
+	
+	public FixedCapacityStackOfStrings(int cap)
+	{
+		a = new String[cap];
+	}
+	
+	void push(String item)
+	{
+		a[N++] = item;
+	}
+	
+	String pop()
+	{
+		return a[--N];
+	}
+	
+	boolean isEmpty()
+	{
+		return N == 0;
+	}
+	
+	int size()
+	{
+		return N;
+	}
+	
+	public static void main(String[] args) 
+	{
+		FixedCapacityStackOfStrings s;
+		s = new FixedCapacityStackOfStrings(100);
+		
+		while (!StdIn.isEmpty())
+		{
+			String item = StdIn.readString();
+			if (!item.equals("-"))
+			{
+				s.push(item);
+			}
+			else if (!s.isEmpty())
+			{
+				StdOut.print(s.pop() + " ");
+			}
+		}
+		
+		StdOut.println("(" + s.size() + " left on stack)");
 	}
 
 }
